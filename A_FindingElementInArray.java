98c89cc02f2f918c6894fce9d49da37a7cd58044
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-01-04 22:50:20 +0530

    A_FindingElementInArray

--- A_FindingElementInArray.java
+++ A_FindingElementInArray.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class FindingElementInAray {
+public class A_FindingElementInArray {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
